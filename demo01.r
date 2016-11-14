381ac50d5efa3eafd7e8206b574d4847684a748a
junlongsun <junlong.sun@gmail.com>
2016-11-14 17:05:05 +0800

    remove comments

--- demo01.r
+++ demo01.r
@@ -1,15 +1,3 @@
-#demo
-
-#beta = matrix(c(0.4, 0.8, 1.3, -0.5, 1.1, 1.2, 1.5, 0.9, 0.2, -0.5), nrow = 2, ncol = 5)
-#set.seed(999)
-#n=50000;
-#n_train = n*0.8;
-#K = 3;
-#x1 = rnorm(n, mean = 0, sd = 1)
-#x2 = rnorm(n, mean = 0, sd = 1)
-#x3 = rnorm(n, mean = 0, sd = 1)
-#x4 = rnorm(n, mean = 0, sd = 1)
-
 library(R.matlab)
 data=readMat('simulation.mat')
 X_train = data$X.train
